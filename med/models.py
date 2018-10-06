c694a5bef903f0fd4fac9aa6d607979160d9daa5
khalilmlk <khalil062010@gmail.com>
2018-10-06 20:32:04 -0300

    Ajuste de envio da Agenda, correção do envio do Json com as consultas.

--- med/models.py
+++ med/models.py
@@ -4,9 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Users(models.Model):
+class Consulta(models.Model):
     codigo = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=255)
-    usuario = models.CharField(max_length=40)
-    senha = models.CharField(max_length=40)
-    email = models.CharField(max_length=150)
+    user_codigo = models.CharField(max_length=10)
+    date = models.CharField(max_length=14)
+    hora = models.CharField(max_length=10)
+    comentario = models.CharField(max_length=200)
--- med/views.py
+++ med/views.py
@@ -1,4 +1,5 @@
 import json
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
@@ -10,40 +11,14 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from med.models import Consulta
 
 
 @csrf_exempt
 @api_view(["GET"])
 @permission_classes((AllowAny,))
 def index(request):
-    return render(request, 'index.html')
-
-
-@csrf_exempt
-@api_view(["POST"])
-@permission_classes((AllowAny,))
-def logar(request):
-    try:
-        usuario_aux = User.objects.get(username=request.POST['username'])
-        usuario = authenticate(username=usuario_aux.username,
-                               password=request.POST["password"])
-        if usuario is not None:
-            try:
-                login(request, usuario)
-                return HttpResponseRedirect('adm')
-            except:
-                return render(request, 'index.html')
-        else:
-            return redirect_index_mensagem(1, request, 'Senha inválida!')
-    except User.DoesNotExist:
-        return redirect_index_mensagem(1, request, 'Usuário inválido!')
-
-
-@csrf_exempt
-@login_required
-def sair(request):
-    logout(request)
-    return HttpResponseRedirect('/')
+    return verifica_login(request, 'index.html')
 
 
 @csrf_exempt
@@ -64,17 +39,107 @@ def cadastro(request):
             novoUsuario.save()
             return redirect_index_mensagem(0, request, 'Cadastro realizado com sucesso.')
     else:
-        return render(request, 'cadastro.html')
+        return verifica_login(request, 'cadastro.html')
+
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def logar(request):
+    try:
+        usuario_aux = User.objects.get(username=request.POST['username'])
+        usuario = authenticate(username=usuario_aux.username,
+                               password=request.POST["password"])
+        if usuario is not None:
+            try:
+                login(request, usuario)
+                return HttpResponseRedirect('home')
+            except:
+                return redirect_index_mensagem(1, request, 'Não foi possivel fazer o login.')
+        else:
+            return redirect_index_mensagem(1, request, 'Senha inválida!')
+    except User.DoesNotExist:
+        return redirect_index_mensagem(1, request, 'Usuário inválido!')
+
+
+@csrf_exempt
+@login_required
+def sair(request):
+    logout(request)
+    return HttpResponseRedirect('/')
 
 
 @csrf_exempt
 @api_view(["GET"])
 @permission_classes((IsAuthenticated,))
 def adm(request):
-    if request.user is not None:
-        return render(request, 'adm.html')
+    return render(request, 'adm.html')
+
+
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((IsAuthenticated,))
+def home(request):
+    return render(request, 'home.html')
+
+
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((IsAuthenticated,))
+def agenda(request):
+    evento = []
+    if True:
+        consultas = Consulta.objects.all().values('codigo', 'user_codigo', 'date', 'hora')
+        for consulta in consultas:
+            eventos = [{
+                "title": "Consulta",
+                "start": "%sT%s" % (consulta['date'], consulta['hora']),
+            }]
+            evento += eventos
+        return HttpResponse(json.dumps(evento), content_type="application/json")
     else:
-        messages.error(request, 'Para acessar, digite login e senha.')
+        return HttpResponse(json.dumps([
+            {
+                "title": "Cheio",
+                "start": "2018-10-05",
+                "end": "2018-10-05"
+            },
+            {
+                "title": "Consulta",
+                "start": "2018-10-05T10:30:00-05:00",
+                "end": "2018-10-05T12:30:00"
+            },
+            {
+                "title": "Consulta",
+                "start": "2018-10-05T12:00:00"
+            },
+            {
+                "title": "Consulta",
+                "start": "2018-10-05T14:30:00"
+            },
+            {
+                "title": "Consulta",
+                "start": "2018-10-05T17:30:00"
+            },
+            {
+                "title": "Consulta",
+                "start": "2018-10-05T20:00:00"
+            }
+        ]), content_type="application/json")
+
+
+@csrf_exempt
+def consulta(request):
+    if request.method == 'POST':
+        data = request.POST['data']
+        time = request.POST['time']
+        comentario = request.POST['comentario']
+        consulta = Consulta(user_codigo=request.user.id, date=data, hora=time, comentario=comentario)
+        consulta.save()
+        messages.success(request, 'Consulta marcada.')
+        return redirect(home)
+    else:
+        messages.error(request, 'Não foi possivel marcar a consulta.')
         return redirect(index)
 
 
@@ -87,3 +152,10 @@ def redirect_index_mensagem(tipo=None, request=None, texto=None):
         return redirect(index)
     else:
         return redirect(index)
+
+
+def verifica_login(request, pagina):
+    if request.user.id is not None:
+        return HttpResponseRedirect('home')
+    else:
+        return render(request, pagina)
