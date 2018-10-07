e6c99f44e7c52f18ba47d2c17b044fc9d0481e4e
khalilmlk <khalil062010@gmail.com>
2018-10-07 07:44:06 -0300

    Altera template para exibir Painel. Adiciona arquivos do bootstrap.

--- templates/js/calendario.js
+++ templates/js/calendario.js
@@ -1,12 +1,38 @@
+var data = new Date();
+var dia     = data.getDate();
+var mes     = data.getMonth();
+var ano4    = data.getFullYear();
+var str_data = ano4 + '-' + (mes+1) + '-' +dia;
+if (dia < 10) {
+    var str_data = ano4 + '-' + (mes+1) + '-0' +dia;
+}else{
+    var str_data = ano4 + '-' + (mes+1) + '-' +dia;
+}
+
 $(document).ready(function() {
+
+    $.getJSON( "consulta", function( data ) {
+      var items = [];
+      console.log(data);
+      $.each( data, function( key, val ) {
+      console.log(val.date);
+        items.push( "<li class=\"list-group-item\" data-color=\"info\" id-user=\"" + val.user_codigo + "\" cod-consulta=\"" + val.codigo + "\" >" + val.date +" - " + val.hora + "</li>" );
+      });
+
+      $( "<ul/>", {
+        'class': 'list-group checked-list-box',
+        html: items.join( "" )
+      }).appendTo( "body" );
+    });
+
     $('#calendar').fullCalendar({
     header: {
     left: 'prev,next today',
     center: 'title',
-    right: 'month,agendaWeek,agendaDay,listWeek'
+    right: 'month'
     },
-    defaultDate: '2018-10-05',
-    editable: true,
+    defaultDate: str_data,
+    editable: false,
     navLinks: true, // can click day/week names to navigate views
     eventLimit: true, // allow "more" link when too many events
     events: {
--- templates/js/calendario_adm.js
+++ templates/js/calendario_adm.js
@@ -1,12 +1,24 @@
+var data = new Date();
+var dia     = data.getDate();
+var mes     = data.getMonth();
+var ano4    = data.getFullYear();
+var str_data = ano4 + '-' + (mes+1) + '-' +dia;
+if (dia < 10) {
+    var str_data = ano4 + '-' + (mes+1) + '-0' +dia;
+}else{
+    var str_data = ano4 + '-' + (mes+1) + '-' +dia;
+}
+
 $(document).ready(function() {
+
     $('#calendar').fullCalendar({
     header: {
     left: 'prev,next today',
     center: 'title',
-    right: 'month,agendaWeek,agendaDay,listWeek'
+    right: 'agendaWeek,agendaDay'
     },
-    defaultDate: '2018-10-05',
-    editable: true,
+    defaultDate: str_data,
+    editable: false,
     navLinks: true, // can click day/week names to navigate views
     eventLimit: true, // allow "more" link when too many events
     events: {
